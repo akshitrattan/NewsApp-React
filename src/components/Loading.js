15e6bba88f0fa2e7cbd130689b1da3ea2d11bf36
Akshit Rattan <akshitrattan@Akshits-MacBook-Air.local>
2022-05-27 20:42:43 +0530

    Added the Page Loading GIF

---
+++ src/components/Loading.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import loading from './Loading.gif';
+
+export default class Loading extends Component {
+  render() {
+    return (
+      <div> 
+        <img src={loading} alt="" className="mx-auto d-block"/>
+      </div>
+    );
+  }
+}
+
+
