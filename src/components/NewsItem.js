5493641d51bf58d2f72097ea97f7ec5a9266498f
Akshit Rattan <akshitrattan@Akshits-MacBook-Air.local>
2022-05-28 12:26:03 +0530

    Centering the news cards

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,8 +5,7 @@ export default class NewsItem extends Component {
     render() {
       let {title, description, imageURL, newsURL} = this.props;
     return (
-      <div>
-        <div className="card my-3" style={{width: '18rem', height: "28rem"}}>
+        <div className="card my-3" style={{width: "18rem", height: "28rem"}}>
             <div className="card-body">
                 <img src={imageURL} style={{width: "100%", height: "200px"}} className="card-img-top" alt="..."/>
                 <h5 className="card-title">{title}...</h5>
@@ -16,7 +15,6 @@ export default class NewsItem extends Component {
                 <a href={newsURL} target="_blank" className="btn btn-sm btn-primary">Read More</a> 
             </div>
         </div>
-      </div>
     );
   } 
 }
